bd02faffbcf21c53916b3ca7c5263da9636d8766
kristiyan <example@example.bg>
2026-08-31 12:24:39 +0300

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { navLinks } from "#constants";
+import dayjs from "dayjs";
+import { navLinks, navIcons } from "#constants";
 
 function Navbar() {
     return (
@@ -13,6 +14,16 @@ function Navbar() {
                         </li>
                     ))}
                 </ul>
+            </div>
+            <div>
+                <ul>
+                    {navIcons.map(({id, img}) => (
+                        <li key={id}>
+                            <img src={img} className="icon-hover" alt={`icon-${id}`}/>
+                        </li>
+                    ))}
+                </ul>
+                <time>{dayjs().format("ddd MMM D h:mm A")}</time>
             </div>
         </nav>
     );
--- src/constants/index.js
+++ src/constants/index.js
@@ -4,4 +4,13 @@ const navLinks = [
     {id: 3, name: "Projects"}
 ];
 
-export { navLinks };
+const navIcons = [
+  { id: 1, img: "/icons/wifi.svg" },
+  { id: 2, img: "/icons/search.svg" },
+  { id: 3, img: "/icons/user.svg" },
+  { id: 4, img: "/icons/mode.svg" },
+];
+
+
+
+export { navLinks, navIcons};
