ecccaaf823de2334a96787f14f6a9e5bed6ba86d
kristiyan <example@example.bg>
2026-09-02 11:23:46 +0300

    Add Resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,38 @@
+import { navIcons, navLinks } from "#constants";
+import useWindowStore from "#stores/window";
 import dayjs from "dayjs";
-import { navLinks, navIcons } from "#constants";
 
 function Navbar() {
+    const { openWindow } = useWindowStore();
+
     return (
         <nav>
             <div>
-                <img src="./images/logo.svg" alt="logo"/>
-                <p className="font-bold">Desktop</p>
-                <ul>
-                    {navLinks.map(({id, name}) => (
-                        <li key={id}>
-                            <p>{name}</p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div>
-                <ul>
-                    {navIcons.map(({id, img}) => (
-                        <li key={id}>
-                            <img src={img} className="icon-hover" alt={`icon-${id}`}/>
-                        </li>
-                    ))}
-                </ul>
-                <time>{dayjs().format("ddd MMM D h:mm A")}</time>
-            </div>
-        </nav>
-    );
-}
+                <img src="/images/logo.svg" alt="Apple Logo" />
+                <p className="font-bold">User</p>
+
+            <ul>
+                {navLinks.map(({ id, name, type }) => (
+                    <li key={id} onClick={() => openWindow(type)}>
+                        <p>{name}</p>
+                    </li>
+                ))}
+            </ul>
+      </div>
+
+      <div>
+            <ul>
+                {navIcons.map(({ id, img }) => (
+                    <li key={id}>
+                        <img src={img} alt={`icon-${id}`} className="icon" />
+                    </li>
+                ))}
+            </ul>
+
+            <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
---
+++ src/windows/index.jsx
@@ -0,0 +1,3 @@
+export { default as ResumeWindow } from "./Resume";
+export { default as SafariWindow } from "./Safari";
+export { default as TerminalWindow } from "./Terminal";
